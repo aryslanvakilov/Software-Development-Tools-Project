bd3b6e50e0e4fe8f982f13111ea8a2d8200d7bf1
Arslan <arslanporshe@gmail.com>
2024-04-15 21:19:58 -0700

    update_app.py

--- app.py
+++ app.py
@@ -2,3 +2,18 @@ import streamlit as st
 import pandas as pd
 
 st.title("Hello")
+
+df = pd.read_csv('vehicles_us.csv')
+
+st.write(df.head())
+st.header('Market for used cars')
+st.write("""##### Filtering data below to see the add by model""")
+show_new_cars = st.checkbox('Include new cars from deakers')
+
+
+if not show_new_cars:
+    df = df[df.condition!='good']
+
+# creating options for filter for all manufacturers 
+manufacturer_choice = df['model'].unique()
+users_choice = st.selectbox('Select model:', manufacturer_choice)
