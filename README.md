8c264861f4cd84b906cd0dcd2e9b83c897c524bb
Arslan <arslanporshe@gmail.com>
2024-04-22 19:38:08 -0700

    Update

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 The car advertisement dataset
 [Link to running app](https://software-development-tools-project-1.onrender.com)
 
+This project aims to provide you with additional practice on common software engineering tasks. These tasks will augment and complement your data skills, and make you a more attractive job candidate to potential employers. 
+
+You will be asked to develop and deploy a web application to a cloud service so that it is accessible to the public.
+
+In this project, we will provide you with the dataset on car sales advertisements, which you’ve already worked with in the past.
+
+The project is split into several steps that replicate the process described in our blog post here, but we will be using the Render platform instead of Heroku.
--- app.py
+++ app.py
@@ -3,25 +3,34 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 
-st.header('Market for used cars')
-st.write('Filter the data below to see the ads by manufacturer')
 
 df = pd.read_csv('vehicles_us.csv')
+
+st.header('Market for used cars')
+st.write("""
+         ##### Filter the data below to see the ads by manufacturer""")
+show_new_cars = st.checkbox('Include new cars from dealers')
+if not show_new_cars:
+    df = df[df.condition!='good']
+
 
 manufacturer_choice = df['model'].unique()
 users_choice = st.selectbox('Select model:', manufacturer_choice)
 
 min_year, max_year = int(df['model_year'].min()), int(df['model_year'].max())
 
+
 df_filtered = df[df.model == users_choice]
 
 year_range = st.slider("Choose years",
                        value=(min_year, max_year),min_value=min_year,max_value=max_year)
+
+show_records = st.number_input("Numbers of records to show", min_value=5, max_value=len(df_filtered))
                        
 actual_range = list(range(year_range[0], year_range[1]+1))
 
 df_filtered = df[(df.model==users_choice) & (df.model_year >= year_range[0]) & (df.model_year <= year_range[1])]
-st.table(df_filtered)
+st.table(df_filtered[:show_records])
 
 st.header('Price analysis')
 st.write("""###### Let's analyze what price influences the most. We'll check how distribution of price varies depending on 
@@ -30,11 +39,16 @@ transmission,cylinders,fuel and type""")
 list_for_hist = ['transmission','cylinders','fuel','type']
 selected_type = st.selectbox('Split for price distribution', list_for_hist)
 
-fig1 = px.histogram(df, x='price', color=selected_type)
-fig1.update_layout(title="<b> Split of price by {}</b>".format(selected_type))
+fig1 = px.histogram(df, x='price', labels={"price": "Price"}, color=selected_type)
+fig1.update_layout(
+    title="<b> Split of price by {}</b>".format(selected_type),
+    yaxis_title="Number of records"
+    )
 
 st.plotly_chart(fig1)
 fig1.show()
+
+
 
 list_for_scatter = ['odometer','paint_color','is_4wd']
 choice_for_scatter = st.selectbox('Price dependency on',list_for_scatter)
